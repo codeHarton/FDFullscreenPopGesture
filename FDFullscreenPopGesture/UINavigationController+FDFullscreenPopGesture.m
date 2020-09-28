e0b69f0b283925a323e2c86abea3f43711907cd1
wangshoudu <wangshoudu@yungengxin.com>
2020-09-28 09:55:30 +0800

    更换 class判断方式

--- FDFullscreenPopGesture/UINavigationController+FDFullscreenPopGesture.m
+++ FDFullscreenPopGesture/UINavigationController+FDFullscreenPopGesture.m
@@ -226,9 +226,8 @@ typedef void (^_FDViewControllerWillAppearInjectBlock)(UIViewController *viewCon
         
         objc_setAssociatedObject(self, _cmd, panGestureRecognizer, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
     }
-    Class MyNavClass = NSClassFromString(@"MyNavigationController");
-    
-    panGestureRecognizer.enabled = [self isMemberOfClass:MyNavClass];
+    NSString *className = NSStringFromClass(self.class);
+    panGestureRecognizer.enabled = [className containsString:@"MyNavigationController"];
     
     return panGestureRecognizer;
 }
